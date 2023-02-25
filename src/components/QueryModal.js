24da108dcd8bf1dcafe903e34338982464fa18a8
Abhishek Sharma <abhishek@convin.ai>
2023-02-25 15:59:20 +0530

    feat: multiple query

--- src/components/QueryModal.js
+++ src/components/QueryModal.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { AiOutlineClose, AiOutlineCopy } from 'react-icons/ai'
 import FilterGroup from './FilterGroup';
 import { useGlobalContext } from '../context/context';
@@ -7,6 +7,23 @@ import { v4 as uuidv4 } from 'uuid';
 
 const QueryModal = () => {
     const { filterGroups, newFilterGroups, defaultRuleGroup, query } = useGlobalContext();
+
+    const [showMore, setShowMore] = useState([]);
+
+    useEffect(() => {
+        let tempShowMore = showMore;
+        for (let i = 0; i < query.length; i++) {
+            if (i >= tempShowMore.length) tempShowMore.push(false);
+        }
+        console.log(tempShowMore);
+        setShowMore([...tempShowMore]);
+    }, [query])
+
+    const toggleMore = (ind) => {
+        let tempShowMore = showMore;
+        tempShowMore[ind] = !tempShowMore[ind];
+        setShowMore([...tempShowMore]);
+    }
 
     const addFilterGroup = () => {
         const temp = [...filterGroups, { ...defaultRuleGroup, id: uuidv4() }];
@@ -20,8 +37,8 @@ const QueryModal = () => {
 
     return (
         <div className='absolute z-10 w-full h-full'>
-            <div className='my-[100px] mx-auto h-[600px] w-[960px] bg-[#1D2025] rounded-md md-[500px]'>
-                <div className='relative w-full bg-[#5C61F0] h-[100px] p-5 text-white rounded-t-md'>
+            <div className='my-[100px] mx-auto h-[640px] w-[960px] bg-[#1D2025] rounded-md md-[500px]'>
+                <div className='relative w-full bg-[#5C61F0] max-h-[140px] p-5 text-white rounded-t-md overflow-hidden'>
                     <div className='flex justify-between'>
                         {
                             (query.length === 0) ? (
@@ -30,19 +47,20 @@ const QueryModal = () => {
                                 <h2 className='text-lg'>Build your query</h2>
                             )
                         }
-                    <button className='absolute right-2 top-2 p-1 bg-[#4338CA] rounded-md'>
-                        <AiOutlineClose color='white' />
-                    </button>
+                        <button className='absolute right-3 top-3 p-1 bg-[#4338CA] rounded-md'>
+                            <AiOutlineClose color='white' />
+                        </button>
                     </div>
-                    <div className='overflow-scroll py-1'>
+                    <div className='py-1 overflow-auto max-h-20'>
                         {
                             (query.length) ? (
                                 query.map((currQuery, index) => {
                                     return (
                                         <div key={index} className='w-full flex gap-3 items-center mb-3'>
-                                            <div className='bg-[#4338CA] w-[80%] py-1 px-2 rounded-md flex justify-between items-center'>
-                                                <p>
-                                                    <span className='font-bold'>Query: </span>
+                                            <div className='bg-[#4338CA] w-[80%] py-1 px-2 rounded-md flex justify-between items-center gap-2'>
+                                                <p
+                                                    className={`${(showMore[index]) ? '' : 'truncate'}`}>
+                                                    <span className='font-bold'>Query {index + 1}: </span>
                                                     {currQuery}
                                                 </p>
                                                 <AiOutlineCopy
@@ -52,38 +70,41 @@ const QueryModal = () => {
                                                     }}
                                                 />
                                             </div>
-                                            <div className='cursor-pointer'>more...</div>
+                                            <div
+                                                className='cursor-pointer'
+                                                onClick={() => toggleMore(index)}
+                                            >more...</div>
                                         </div>
-                                    )
+                    )
                                 })
-                            ) : (
-                                <p className='opacity-50 text-sm'>The query you build will be saved in your active view</p>
-                            )
+                    ) : (
+                    <p className='opacity-50 text-sm'>The query you build will be saved in your active view</p>
+                    )
                         }
-                    </div>
                 </div>
-                <div className='h-[500px] flex p-5 z-30 rounded-b-md  overflow-scroll'>
-                    <div className='w-full h-fit relative max-h-fit'>
-                        {
-                            filterGroups.map((group, index) => {
-                                const { id } = group;
-                                return (
-                                    <FilterGroup key={id} index={index} group={group} />
-                                )
-                            })
-                        }
-                        <div className=' text-white mb-10'>
-                            <div className='h-10 w-[1px] bg-[#5C61F0] ml-10'></div>
-                            <button
-                                className='py-2 px-4 bg-[#5C61F0] rounded-md'
-                                onClick={addFilterGroup}
-                            >
-                                + Add New Group Filter
-                            </button>
-                        </div>
+            </div>
+            <div className='h-[500px] flex p-5 z-30 rounded-b-md  overflow-scroll'>
+                <div className='w-full h-fit relative max-h-fit'>
+                    {
+                        filterGroups.map((group, index) => {
+                            const { id } = group;
+                            return (
+                                <FilterGroup key={id} index={index} group={group} />
+                            )
+                        })
+                    }
+                    <div className=' text-white mb-10'>
+                        <div className='h-10 w-[1px] bg-[#5C61F0] ml-10'></div>
+                        <button
+                            className='py-2 px-4 bg-[#5C61F0] rounded-md'
+                            onClick={addFilterGroup}
+                        >
+                            + Add New Group Filter
+                        </button>
                     </div>
                 </div>
             </div>
+        </div>
         </div >
     )
 }
